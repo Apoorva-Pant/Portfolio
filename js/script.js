6989ced5e4dd04b224122e5c941c0b1961b229c8
Apurv Pant <apoorvavijaypant13@gmail.com>
2025-04-26 13:28:48 +0530

    Updated JS

--- js/script.js
+++ js/script.js
@@ -1,29 +1,46 @@
 console.log("Portfolio loaded successfully.");
-const toggleBtn = document.getElementById("theme-toggle");
-let isDark = false;
 
-toggleBtn.addEventListener("click", () => {
-  document.documentElement.style.setProperty('--bg-color', isDark ? '#f9f9f9' : '#1e1e1e');
-  document.documentElement.style.setProperty('--text-color', isDark ? '#111' : '#f9f9f9');
-  document.documentElement.style.setProperty('--card-bg', isDark ? '#fff' : '#2c2c2c');
-  document.documentElement.style.setProperty('--accent-color', isDark ? '#6c63ff' : '#0099ff');
-  isDark = !isDark;
-});
+
 document.addEventListener("DOMContentLoaded", () => {
+
+
   const loader = document.getElementById("loader");
-  loader.classList.add("fade-out");
-  setTimeout(() => loader.remove(), 500);
-});
-  const typeText = document.getElementById("type-text");
-const textToType = "Hello, I’m Apoorva 👋";
-let i = 0;
-
-function typeWriter() {
-  if (i < textToType.length) {
-    typeText.innerHTML += textToType.charAt(i);
-    i++;
-    setTimeout(typeWriter, 80);
+  if (loader) {
+    loader.classList.add("fade-out");
+    setTimeout(() => {
+      loader.remove();
+    }, 500);
   }
-}
 
-window.addEventListener("load", typeWriter);
+ 
+  const toggleBtn = document.getElementById("theme-toggle");
+  let isDark = false;
+
+  if (toggleBtn) {
+    toggleBtn.addEventListener("click", () => {
+      document.documentElement.style.setProperty('--bg-color', isDark ? '#f9f9f9' : '#1e1e1e');
+      document.documentElement.style.setProperty('--text-color', isDark ? '#111' : '#f9f9f9');
+      document.documentElement.style.setProperty('--card-bg', isDark ? '#fff' : '#2c2c2c');
+      document.documentElement.style.setProperty('--accent-color', isDark ? '#6c63ff' : '#0099ff');
+      isDark = !isDark;
+    });
+  }
+
+  
+  const typeText = document.getElementById("type-text");
+  const textToType = "Welcome to my Portfolio👋";
+  let i = 0;
+
+  function typeWriter() {
+    if (i < textToType.length) {
+      typeText.innerHTML += textToType.charAt(i);
+      i++;
+      setTimeout(typeWriter, 80);
+    }
+  }
+
+  if (typeText) {
+    typeWriter();
+  }
+
+});
